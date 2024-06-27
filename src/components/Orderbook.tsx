077f973901743635cf8f63faa7acff698f41c849
fullstack <felix.ma1991823@gmail.com>
2024-06-27 02:28:13 -0400

    show basic data using orderbook api

--- src/components/Orderbook.tsx
+++ src/components/Orderbook.tsx
@@ -1,5 +1,50 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export const Orderbook = () => {
+  const [bids, setBids] = useState([]);
+  const [asks, setAsks] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.prod.rabbitx.io/markets/orderbook", {
+        params: {
+          market_id: "BTC-USD",
+          p_limit: 100,
+          p_page: 0,
+          p_order: "DESC",
+        },
+      })
+      .then((response) => {
+        if (response.data.success) {
+          console.log("response =>", response.data.result[0]);
+          setBids(response.data.result[0].bids);
+          setAsks(response.data.result[0].asks);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <div>Test</div>
-  )
-}
+    <div className="bg-cyan-950 p-4 min-h-96 w-80">
+      {bids.map((bid, index) => {
+        return (
+          <div key={index} className="flex justify-between">
+            <span className="text-white">{bid[0]}</span>
+            <span className="text-white">{bid[1]}</span>
+          </div>
+        );
+      })}
+      {asks.map((ask, index) => {
+        return (
+          <div key={index} className="flex justify-between">
+            <span className="text-white">{ask[0]}</span>
+            <span className="text-white">{ask[1]}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
