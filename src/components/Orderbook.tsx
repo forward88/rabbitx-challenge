0bc63b064c9d25314211dadcd25cda49f2606c39
fullstack <felix.ma1991823@gmail.com>
2024-06-27 05:53:00 -0400

    added centrifuge

--- src/components/Orderbook.tsx
+++ src/components/Orderbook.tsx
@@ -1,5 +1,13 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Centrifuge } from "centrifuge";
+
+const JWT_TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0MDAwMDAwMDAwIiwiZXhwIjo2NTQ4NDg3NTY5fQ.o_qBZltZdDHBH3zHPQkcRhVBQCtejIuyq8V1yj5kYq8";
+
+const centrifuge = new Centrifuge("wss://api.prod.rabbitx.io/ws", {
+  token: JWT_TOKEN,
+});
 
 export const Orderbook = () => {
   const [bids, setBids] = useState([]);
@@ -25,11 +33,24 @@ export const Orderbook = () => {
       .catch((error) => {
         console.log(error);
       });
+
+    const sub = centrifuge.newSubscription("orderbook");
+
+    // React on `news` channel real-time publications.
+    sub.on("publication", function (ctx) {
+      console.log(ctx.data);
+    });
+
+    // Trigger subscribe process.
+    sub.subscribe();
+
+    // Trigger actual connection establishement.
+    centrifuge.connect();
   }, []);
 
   return (
-    <div className="bg-cyan-950 p-4 min-h-96 w-80">
-      <div className="font-bold text-3xl text-white">Bids</div>
+    <div className="bg-cyan-950 p-4 min-h-96 w-80 rounded-lg">
+      <div className="font-bold text-sm text-white">Orderbook</div>
       {bids.slice(0, 11).map((bid, index) => {
         return (
           <div key={index} className="flex justify-between">
@@ -38,7 +59,9 @@ export const Orderbook = () => {
           </div>
         );
       })}
-      <div className="font-bold text-3xl text-white">Asks</div>
+      <div className="font-bold text-base text-white bg-cyan-900 p-2 my-2">
+        60,875
+      </div>
       {asks.slice(0, 11).map((ask, index) => {
         return (
           <div key={index} className="flex justify-between">
